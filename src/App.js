703e349d0c6e0b4d9b3f75e4c44c73486f8a3737
Durgesh Kumar Singh <93372022+Durgesh2601@users.noreply.github.com>
2024-05-24 22:40:02 +0530

    feat: UI components (#1)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import ReactFlow, {
+  ReactFlowProvider,
+  addEdge,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+} from "reactflow";
+import "reactflow/dist/style.css";
+import NodesPanel from "./components/NodesPanel";
+import Topbar from "./components/Topbar";
+import "./App.css";
+import CustomAlert from "./components/CustomAlert";
+import CustomTextNode from "./components/CustomNodeRenderer";
+import { initialNodes } from "./constants";
+import { getId } from "./utils";
+
+const initialEdges = [];
+
+const nodeTypes = { textNode: CustomTextNode };
 
 function App() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [selectedNode, setSelectedNode] = useState(null);
+  const [reactFlowInstance, setReactFlowInstance] = useState(null);
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  const onNodeDragStop = (event, node) =>
+    setNodes((nds) => nds.map((n) => (n.id === node.id ? node : n)));
+
+  const onNodesDelete = (selectedNodes) => {
+    const newNodes = nodes.filter((node) => !selectedNodes.includes(node.id));
+    const newEdges = edges.filter(
+      (edge) =>
+        !selectedNodes.includes(edge.source) &&
+        !selectedNodes.includes(edge.target)
+    );
+    setNodes(newNodes);
+    setEdges(newEdges);
+    setSelectedNode(null);
+  };
+
+  const onDragOver = (event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  };
+
+  const onDrop = (event) => {
+    event.preventDefault();
+    const type = event.dataTransfer.getData("application/reactflow");
+    // check if the dropped element is valid
+    if (typeof type === "undefined" || !type) {
+      return;
+    }
+    const position = reactFlowInstance.screenToFlowPosition({
+      x: event.clientX,
+      y: event.clientY,
+    });
+    const id = getId(nodes);
+    const newNode = {
+      id,
+      type,
+      position,
+      data: { label: `test message ${id}` },
+    };
+    setNodes((prevNodes) => prevNodes.concat(newNode));
+  };
+
+  const handleUpdateNodeLabel = (nodeId, label) => {
+    const mappedNodes = nodes.map((node) => {
+      if (node.id === nodeId) {
+        return {
+          ...node,
+          data: {
+            ...node.data,
+            label,
+          },
+        };
+      }
+      return node;
+    });
+    setNodes(mappedNodes);
+    setSelectedNode(null);
+  };
+
+  const closeAlert = () => {
+    setAlertMessage("");
+  };
+
+  const handleSave = useCallback(() => {
+    const nodesWithoutTarget = nodes.filter(
+      (node) => !edges.find((edge) => edge.source === node.id)
+    );
+    if (nodesWithoutTarget.length > 1) {
+      setAlertMessage(
+        "Cannot save flow! More than one node with empty target handles"
+      );
+    } else {
+      setAlertMessage("Flow saved successfully!");
+      console.log("Flow saved");
+    }
+  }, [nodes]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Topbar handleSave={handleSave} />
+      <div className="flow-builder">
+        <ReactFlowProvider>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            elements={[...nodes, ...edges]}
+            onConnect={onConnect}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onNodeDragStop={onNodeDragStop}
+            onNodeClick={(event, node) => setSelectedNode(node)}
+            onNodesDelete={onNodesDelete}
+            onInit={setReactFlowInstance}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
+            nodeTypes={nodeTypes}
+          >
+            <Controls />
+            <Background />
+          </ReactFlow>
+          <NodesPanel
+            selectedNode={selectedNode}
+            setSelectedNode={setSelectedNode}
+            handleUpdateNodeLabel={handleUpdateNodeLabel}
+          />
+          {alertMessage && (
+            <CustomAlert message={alertMessage} onClose={closeAlert} />
+          )}
+        </ReactFlowProvider>
+      </div>
     </div>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const initialNodes = [
+  {
+    id: "1",
+    type: "default",
+    position: { x: 100, y: 100 },
+    data: { label: "test message 1" },
+    type: "textNode",
+  },
+];
